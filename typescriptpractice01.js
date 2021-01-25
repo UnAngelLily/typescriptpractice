bdadb5fdf0655e8d7e3afbeb32cc09f20503bbab
UnAngelLily <unangellily@gmail.com>
2021-01-25 13:13:54 -0700

    correcting errors via the terminal tsc

--- typescriptpractice01.js
+++ typescriptpractice01.js
@@ -4,6 +4,6 @@ var input2 = document.getElementById("num2");
 function add(num1, num2) {
     return num1 + num2;
 }
-buton.addEventListener("click", function () {
-    console.log(add(input1.value, input2.value));
+button.addEventListener("click", function () {
+    console.log(add(+input1.value, +input2.value));
 });
--- typescriptpractice01.ts
+++ typescriptpractice01.ts
@@ -6,6 +6,6 @@ function add(num1: number,  num2: number) {
     return num1 + num2;
 }
 
-buton.addEventListener("click", function() {
-    console.log(add(input1.value, input2.value));
+button.addEventListener("click", function() {
+    console.log(add(+input1.value, +input2.value));
 });
