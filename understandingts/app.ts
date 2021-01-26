181a169e38c143ff0fb26a4efe0d07fce9266161
UnAngelLily <unangellily@gmail.com>
2021-01-26 12:48:02 -0700

    added key values in const variable person

--- understandingts/app.ts
+++ understandingts/app.ts
@@ -1,4 +1,8 @@
-const person: {} = {
+const person: {
+    // key values can be stored in curly brackets
+    name: string;
+    age: number
+} = {
     // {} is typescript's specialized notation for showing an object type instead of 
     // const person: object = 
     name: 'Theo',
