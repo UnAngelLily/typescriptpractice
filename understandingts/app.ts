fefed3cffee2e96aa9504c3ae37a4516fb2157d9
UnAngelLily <unangellily@gmail.com>
2021-01-27 16:55:42 -0700

    enums, editing role and for loop and if loop

--- understandingts/app.ts
+++ understandingts/app.ts
@@ -60,16 +60,25 @@
 //         descript: string;
 //     }
 // }
-
+const ADMIN = 0;
+const READ_ONLY = 1;
+const AUTHOR = 2;
 const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    rolde: [number, string]
-} = {
     name: 'Maximilian',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: ADMIN
+};
 
-;}
+let favoriteActivities: string [];
+favoriteActivies = ['Sports'];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
+
+if (person.role === 'READ-ONLY-USER') {
+    console.log('is read only');
+}
