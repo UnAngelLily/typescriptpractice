2af46669fec88158de7efa1fb57f2cb0210cbfc9
UnAngelLily <unangellily@gmail.com>
2021-01-26 17:02:05 -0700

    commented out longer syntax, typescript dry code shown

--- understandingts/app.ts
+++ understandingts/app.ts
@@ -1,12 +1,17 @@
-const person: {
-    // key values can be stored in curly brackets
-    name: string;
-    age: number
-} = {
+// const person: {
+//     // key values can be stored in curly brackets
+//     name: string;
+//     age: number;
+// } = {
     // {} is typescript's specialized notation for showing an object type instead of 
     // const person: object = 
+//     name: 'Theo',
+//     age: 0
+// };
+
+const person = {
     name: 'Theo',
     age: 0
 };
-
+// better syntax
 console.log(person.name);
