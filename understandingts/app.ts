682187f17a314f7115cd6d35f0a5d473bfa4d967
UnAngelLily <unangellily@gmail.com>
2021-01-28 16:16:26 -0700

    more enum practice

--- understandingts/app.ts
+++ understandingts/app.ts
@@ -67,28 +67,41 @@
 // const READ_ONLY = 1;
 // const AUTHOR = 2;
 
-enum Role {ADMIN, READ_ONLY, AUTHOR};
-// enum is a custom type so convention is to start with captial, often you'll see enums with all-uppercase values but that's not a must-do. You can go with any value names.
-// call assign the role with a different numbering than 0,1,2 etc... 
-    // enum Role { ADMIN = 5, READ_ONLY, AUTHOR }' this becomes 5, 6, 7 instead or you can assign all of them or text or even mix it with a string
-    // enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR };
-const person = {
-    name: 'Maximilian',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
+// enum Role {ADMIN, READ_ONLY, AUTHOR};
+// // enum is a custom type so convention is to start with captial, often you'll see enums with all-uppercase values but that's not a must-do. You can go with any value names.
+// // call assign the role with a different numbering than 0,1,2 etc... 
+//     // enum Role { ADMIN = 5, READ_ONLY, AUTHOR }' this becomes 5, 6, 7 instead or you can assign all of them or text or even mix it with a string
+//     // enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR };
+// const person = {
+//     name: 'Maximilian',
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: Role.ADMIN
+// };
+
+// let favoriteActivities: string [];
+// favoriteActivities = ['Sports'];
+
+// console.log(person.name);
+
+// for (const hobby of person.hobbies) {
+//     console.log(hobby.toUpperCase());
+// }
+
+// if (person.role === Role.AUTHOR) {
+//     // this is refering the enum which assigns labels to numbers.
+//     console.log('is author');
+// }
+
+// ==================== enum ===============================
+
+const person: {
+    name: string;
+    age: number;
+    hobbies: string [];
+} = {
+    name: 'Theo',
+    age: 0,
+    hobbies: ['farting', 'crying'],
+    role: [2, 'baby']
 };
-
-let favoriteActivities: string [];
-favoriteActivities = ['Sports'];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase());
-}
-
-if (person.role === Role.AUTHOR) {
-    // this is refering the enum which assigns labels to numbers.
-    console.log('is author');
-}
